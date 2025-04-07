db7c2c7bb8143eec46d68c825b9b85f6772f7aff
Vektor19 <viktor.komissarov@knu.ua>
2025-04-07 15:27:13 +0300

    feat: create QuestionOption class

---
+++ ExamSystem.Backend/ExamSystem.Core/Entities/QuestionOption.cs
@@ -0,0 +1,11 @@
+﻿namespace ExamSystem.Core.Entities
+{
+    public class QuestionOption
+    {
+        public Guid QuestionOptionId { get; set; }
+        public Guid QuestionId { get; set; }
+        public string Label { get; set; } = string.Empty;
+        public string OptionText { get; set; } = string.Empty;
+        public bool IsCorrect { get; set; }
+    }
+}
