7e95d498ed606e79213fcba1fd75f9aae8c62888
Vektor19 <viktor.komissarov@knu.ua>
2025-04-07 15:03:47 +0300

    feat: create Exam model

---
+++ ExamSystem.Backend/ExamSystem.Core/Entities/Exam.cs
@@ -0,0 +1,15 @@
+﻿using ExamSystem.Core.Enums;
+
+namespace ExamSystem.Core.Entities
+{
+    public class Exam
+    {
+        public Guid ExamId { get; set; }
+        public Guid CreatedByUserId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public ExamStatus Status { get; set; }
+    }
+}
