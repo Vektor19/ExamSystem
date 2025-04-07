ce6668ad015bdddcc6395ea3c8412fb1e28a166f
Vektor19 <viktor.komissarov@knu.ua>
2025-04-07 15:10:30 +0300

    feat: create Question model

---
+++ ExamSystem.Backend/ExamSystem.Core/Entities/Question.cs
@@ -0,0 +1,13 @@
+﻿using ExamSystem.Core.Enums;
+
+namespace ExamSystem.Core.Entities
+{
+    public class Question
+    {
+        public Guid QuestionId { get; set; }
+        public Guid ExamId { get; set; }
+        public string QuestionText { get; set; } = string.Empty;
+        public QuestionType Type { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
