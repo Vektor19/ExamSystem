e8700123d98e928c78c141cea833085dda110995
Vektor19 <viktor.komissarov@knu.ua>
2025-04-07 15:29:25 +0300

    feat: create Answer model

---
+++ ExamSystem.Backend/ExamSystem.Core/Entities/Answer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSystem.Core.Entities
+{
+    public class Answer
+    {
+        public Guid AnswerId { get; set; }
+        public Guid UserId { get; set; }
+        public Guid QuestionOptionId { get; set; }
+
+    }
+}
