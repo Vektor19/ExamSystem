91eeb00e54d08bc75199021e8db10f30d02c1637
Vektor19 <viktor.komissarov@knu.ua>
2025-04-11 21:41:51 +0300

    feat: create QuestionOptionConfiguration

---
+++ ExamSystem.Backend/ExamSystem.Persistence/Configurations/QuestionOptionConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using ExamSystem.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExamSystem.Persistence.Configurations
+{
+    public class QuestionOptionConfiguration : IEntityTypeConfiguration<QuestionOption>
+    {
+        public void Configure(EntityTypeBuilder<QuestionOption> builder)
+        {
+            builder.HasKey(qo => qo.QuestionOptionId);
+
+            builder.HasOne(qo => qo.Question)
+                .WithMany(q => q.QuestionOptions)
+                .HasForeignKey(qo => qo.QuestionId);
+
+            builder.HasMany(qo => qo.Answers)
+                .WithOne(a => a.QuestionOption)
+                .HasForeignKey(a => a.QuestionOptionId);
+        }
+    }
+}
