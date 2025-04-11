feb3a3ee9240a1901a5d24aadebedfe3744f30df
Vektor19 <viktor.komissarov@knu.ua>
2025-04-11 20:15:08 +0300

    feat: add ExamUser entity

---
+++ ExamSystem.Backend/ExamSystem.Core/Entities/ExamUser.cs
@@ -0,0 +1,11 @@
+﻿namespace ExamSystem.Core.Entities
+{
+    public class ExamUser
+    {
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public Guid ExamId { get; set; }
+        public Exam Exam { get; set; }
+        public bool CompleteStatus { get; set; }
+    }
+}
