824484762d92a1abd5f081bb01a6a9d7d75a601b
Vektor19 <viktor.komissarov@knu.ua>
2025-04-11 20:39:09 +0300

    feat: create ExamSystemDbContext, add DbSets

--- ExamSystem.Backend/ExamSystem.Persistence/ExamSystemDbContext.cs
+++ ExamSystem.Backend/ExamSystem.Persistence/ExamSystemDbContext.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ExamSystem.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExamSystem.Persistence
 {
-    public class ExamSystemDbContext
+    public class ExamSystemDbContext: DbContext
     {
+        public ExamSystemDbContext(DbContextOptions<ExamSystemDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<Exam> Exams { get; set; }
+        public DbSet<ExamUser> ExamUsers { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<QuestionOption> QuestionOptions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
