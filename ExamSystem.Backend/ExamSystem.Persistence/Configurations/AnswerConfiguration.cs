df48fbbcf30cb5f6c4f94f26e82cf6a7fb8d4a15
Vektor19 <viktor.komissarov@knu.ua>
2025-04-11 21:44:17 +0300

    feat: create AnswerConfiguration

---
+++ ExamSystem.Backend/ExamSystem.Persistence/Configurations/AnswerConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using ExamSystem.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExamSystem.Persistence.Configurations
+{
+    public class AnswerConfiguration : IEntityTypeConfiguration<Answer>
+    {
+        public void Configure(EntityTypeBuilder<Answer> builder)
+        {
+            builder.HasKey(a => a.AnswerId);
+
+            builder.HasOne(a => a.User)
+                .WithMany(u => u.Answers)
+                .HasForeignKey(a => a.UserId);
+
+            builder.HasOne(a => a.QuestionOption)
+                .WithMany(qo => qo.Answers)
+                .HasForeignKey(a => a.QuestionOptionId);
+        }
+    }
+}
