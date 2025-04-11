99ea5fc3df1e95bacd373635860154283c189f62
Vektor19 <viktor.komissarov@knu.ua>
2025-04-11 21:14:40 +0300

    feat: create RoleConfiguration

---
+++ ExamSystem.Backend/ExamSystem.Persistence/Configurations/RoleConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using ExamSystem.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExamSystem.Persistence.Configurations
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.HasKey(r => r.RoleId);
+
+            builder.HasMany(r => r.UserRoles)
+                .WithOne(ur => ur.Role)
+                .HasForeignKey(ur => ur.RoleId);
+        }
+    }
+}
