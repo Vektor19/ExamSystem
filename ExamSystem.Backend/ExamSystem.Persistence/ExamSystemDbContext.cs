f6062f11a684c6ea5877d6598731badae4196829
Vektor19 <viktor.komissarov@knu.ua>
2025-04-11 21:46:27 +0300

    feat: apply all configurations in db context

--- ExamSystem.Backend/ExamSystem.Persistence/ExamSystemDbContext.cs
+++ ExamSystem.Backend/ExamSystem.Persistence/ExamSystemDbContext.cs
@@ -2,26 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using ExamSystem.Core.Entities;
+using ExamSystem.Persistence.Configurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace ExamSystem.Persistence
 {
-    public class ExamSystemDbContext: DbContext
+    public class ExamSystemDbContext : DbContext
     {
         public ExamSystemDbContext(DbContextOptions<ExamSystemDbContext> options) : base(options)
         {
         }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Role> Roles { get; set; }
-        public DbSet<UserRole> UserRoles { get; set; }
-        public DbSet<Exam> Exams { get; set; }
-        public DbSet<ExamUser> ExamUsers { get; set; }
-        public DbSet<Question> Questions { get; set; }
-        public DbSet<QuestionOption> QuestionOptions { get; set; }
-        public DbSet<Answer> Answers { get; set; }
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Role> Roles { get; set; } = null!;
+        public DbSet<UserRole> UserRoles { get; set; } = null!;
+        public DbSet<Exam> Exams { get; set; } = null!;
+        public DbSet<ExamUser> ExamUsers { get; set; } = null!;
+        public DbSet<Question> Questions { get; set; } = null!;
+        public DbSet<QuestionOption> QuestionOptions { get; set; } = null!;
+        public DbSet<Answer> Answers { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+            modelBuilder.ApplyConfiguration(new UserRoleConfiguration());
+            modelBuilder.ApplyConfiguration(new ExamConfiguration());
+            modelBuilder.ApplyConfiguration(new ExamUserConfiguration());
+            modelBuilder.ApplyConfiguration(new QuestionConfiguration());
+            modelBuilder.ApplyConfiguration(new QuestionOptionConfiguration());
+            modelBuilder.ApplyConfiguration(new AnswerConfiguration());
             base.OnModelCreating(modelBuilder);
         }
     }
