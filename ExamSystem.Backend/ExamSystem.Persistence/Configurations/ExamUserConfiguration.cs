e2884b5e9db2e9e9a41bddd45fbe57454a25cdee
Vektor19 <viktor.komissarov@knu.ua>
2025-04-11 21:33:49 +0300

    feat: create ExamUserConfiguration

---
+++ ExamSystem.Backend/ExamSystem.Persistence/Configurations/ExamUserConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using ExamSystem.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExamSystem.Persistence.Configurations
+{
+    public class ExamUserConfiguration : IEntityTypeConfiguration<ExamUser>
+    {
+        public void Configure(EntityTypeBuilder<ExamUser> builder)
+        {
+            builder.HasKey(eu => eu.ExamUserId);
+
+            builder.HasOne(eu => eu.User)
+                .WithMany(u => u.ExamUsers)
+                .HasForeignKey(eu => eu.UserId);
+
+            builder.HasOne(eu => eu.Exam)
+                .WithMany(e => e.ExamUsers)
+                .HasForeignKey(eu => eu.ExamId);
+
+            builder.HasIndex(eu => new { eu.UserId, eu.ExamId })
+                .IsUnique();
+        }
+    }
+}
