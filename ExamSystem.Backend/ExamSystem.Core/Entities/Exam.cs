73f626b06ef6b37ce85a270bb429d39bc9eadc61
Vektor19 <viktor.komissarov@knu.ua>
2025-04-11 21:28:13 +0300

    fix: remove CreatedExams propery from Exam

--- ExamSystem.Backend/ExamSystem.Core/Entities/Exam.cs
+++ ExamSystem.Backend/ExamSystem.Core/Entities/Exam.cs
@@ -13,7 +13,6 @@ namespace ExamSystem.Core.Entities
         public ExamStatus Status { get; set; }
         public User UserCreatedBy { get; set; } = null!;
         public List<ExamUser> ExamUsers { get; set; } = [];
-        public List<Exam> CreatedExams { get; set; } = [];
         public List<Question> Questions { get; set; } = [];
     }
 }
