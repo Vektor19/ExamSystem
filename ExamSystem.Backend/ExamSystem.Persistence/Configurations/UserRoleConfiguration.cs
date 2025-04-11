1055105c7d17069c3f38c0ab9aa3b194ba611e88
Vektor19 <viktor.komissarov@knu.ua>
2025-04-11 21:18:29 +0300

    feat: create UserRoleConfiguration

---
+++ ExamSystem.Backend/ExamSystem.Persistence/Configurations/UserRoleConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using ExamSystem.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExamSystem.Persistence.Configurations
+{
+    public class UserRoleConfiguration : IEntityTypeConfiguration<UserRole>
+    {
+        public void Configure(EntityTypeBuilder<UserRole> builder)
+        {
+            builder.HasKey(ur => ur.UserRoleId);
+
+            builder.HasOne(ur => ur.User)
+                .WithMany(u => u.UserRoles)
+                .HasForeignKey(ur => ur.UserId);
+
+            builder.HasOne(ur => ur.Role)
+                .WithMany(r => r.UserRoles)
+                .HasForeignKey(ur => ur.RoleId);
+
+            builder.HasIndex(ur => new { ur.UserId, ur.RoleId })
+                .IsUnique();
+        }
+    }
+}
