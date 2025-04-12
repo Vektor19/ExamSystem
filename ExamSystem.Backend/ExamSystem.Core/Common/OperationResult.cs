1af92f80124d6eeee53720b661762f826f4e66df
Vektor19 <viktor.komissarov@knu.ua>
2025-04-13 00:05:20 +0300

    feat: create OperationResult generic version

--- ExamSystem.Backend/ExamSystem.Core/Common/OperationResult.cs
+++ ExamSystem.Backend/ExamSystem.Core/Common/OperationResult.cs
@@ -12,4 +12,18 @@
         public static OperationResult Ok() => new(true);
         public static OperationResult Fail(string error) => new(false, error);
     }
+    public class OperationResult<T>
+    {
+        public bool Success { get; private set; }
+        public string? ErrorMessage { get; private set; }
+        public T? Data { get; private set; }
+        private OperationResult(bool success, T? data = default, string? errorMessage = null)
+        {
+            Success = success;
+            Data = data;
+            ErrorMessage = errorMessage;
+        }
+        public static OperationResult<T> Ok(T data) => new(true, data);
+        public static OperationResult<T> Fail(string error) => new(false, default, error);
+    }
 }
