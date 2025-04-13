014a946c584dfca51faa2354f77df1c356cc9796
Vektor19 <viktor.komissarov@knu.ua>
2025-04-13 15:18:36 +0300

    feat: create AnswerRepository which implements IAnswerRepository

---
+++ ExamSystem.Backend/ExamSystem.Persistence/Repositories/AnswerRepository.cs
@@ -0,0 +1,63 @@
+﻿using ExamSystem.Core.Common;
+using ExamSystem.Core.Entities;
+using ExamSystem.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamSystem.Persistence.Repositories
+{
+    public class AnswerRepository : IAnswerRepository
+    {
+        private readonly ExamSystemDbContext _dbContext;
+        public AnswerRepository(ExamSystemDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+        public async Task<OperationResult<IEnumerable<Answer>>> GetAllAsync()
+        {
+            var exams = await _dbContext.Answers.Include(a => a.QuestionOption)
+                                                .Include(a => a.User)
+                                                .ToListAsync();
+            return OperationResult<IEnumerable<Answer>>.Ok(exams);
+        }
+        public async Task<OperationResult<Answer>> GetByIdAsync(Guid id)
+        {
+            var answer = await _dbContext.Answers.Include(a => a.QuestionOption)
+                                                 .Include(a => a.User)
+                                                 .FirstOrDefaultAsync();
+            if (answer == null)
+                return OperationResult<Answer>.Fail("Answer not found.");
+            return OperationResult<Answer>.Ok(answer);
+        }
+
+        public async Task<OperationResult> AddAsync(Answer entity)
+        {
+            await _dbContext.Answers.AddAsync(entity);
+            var result = await _dbContext.SaveChangesAsync();
+
+            if (result == 0)
+                return OperationResult.Fail("Failed to add answer.");
+
+            return OperationResult.Ok();
+        }
+        public async Task<OperationResult> UpdateAsync(Answer entity)
+        {
+            _dbContext.Answers.Update(entity);
+            var result = await _dbContext.SaveChangesAsync();
+            if (result == 0)
+                return OperationResult.Fail("Failed to update answer.");
+            return OperationResult.Ok();
+        }
+
+        public async Task<OperationResult> DeleteAsync(Guid id)
+        {
+            var answer = await _dbContext.Answers.FindAsync(id);
+            if (answer == null)
+                return OperationResult.Fail("Answer not found.");
+            _dbContext.Answers.Remove(answer);
+            var result = await _dbContext.SaveChangesAsync();
+            if (result == 0)
+                return OperationResult.Fail("Failed to delete answer.");
+            return OperationResult.Ok();
+        }
+    }
+}
