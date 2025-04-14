c72b735775556fad2f745ef8e1e3a1627b5729c6
Vektor19 <viktor.komissarov@knu.ua>
2025-04-14 22:32:20 +0300

    feat: create IUserService interface

---
+++ ExamSystem.Backend/ExamSystem.Application/Interfaces/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using ExamSystem.Application.DTOs;
+
+namespace ExamSystem.Application.Interfaces.Services
+{
+    public interface IUserService
+    {
+        Task<UserDto> GetByIdAsync(Guid id);
+        Task<IEnumerable<UserDto>> GetAllAsync();
+        Task<UserDto> GetByEmailAsync(string email);
+        Task<bool> UpdateAsync(UserDto userDto);
+        Task<bool> DeleteAsync(Guid id);
+        Task<bool> ValidateCredentialsAsync(string email, string password);
+        Task<bool> CreateUserAsync(RegisterUserDto userDto);
+    }
+}
