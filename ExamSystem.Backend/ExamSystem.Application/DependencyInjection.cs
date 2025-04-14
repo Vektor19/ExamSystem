ff6c5c9620ac6308c244e552365c099836480abe
Vektor19 <viktor.komissarov@knu.ua>
2025-04-14 22:37:23 +0300

    feat: add DependencyInjection static class with extension methid to configure JWT auth

---
+++ ExamSystem.Backend/ExamSystem.Application/DependencyInjection.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using ExamSystem.Application.Interfaces.Services;
+using ExamSystem.Application.Models;
+using ExamSystem.Application.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ExamSystem.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            //services.AddScoped<IUserService, UserService>();
+            return services;
+        }
+
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettingsSection = configuration.GetSection("JwtSettings");
+            services.Configure<JwtSettings>(jwtSettingsSection);
+            var jwtSettings = jwtSettingsSection.Get<JwtSettings>();
+            if (jwtSettings == null || string.IsNullOrWhiteSpace(jwtSettings.Secret))
+            {
+                throw new InvalidOperationException("JWT settings are not properly configured.");
+            }
+
+            services.AddScoped<IJwtService, JwtService>();
+            services.AddScoped<IAuthService, AuthService>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret)),
+                    ValidateIssuer = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidateAudience = true,
+                    ValidAudience = jwtSettings.Audience,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            services.AddAuthorization();
+
+            return services;
+        }
+    }
+
+}
