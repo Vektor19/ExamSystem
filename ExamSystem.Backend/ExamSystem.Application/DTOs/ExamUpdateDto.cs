9b4fb4658163eb02b12e20cde2d84b00b97358df
Vektor19 <viktor.komissarov@knu.ua>
2025-04-14 19:30:31 +0300

    feat: ExamUpdateDto

---
+++ ExamSystem.Backend/ExamSystem.Application/DTOs/ExamUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSystem.Application.DTOs
+{
+    public class ExamUpdateDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
