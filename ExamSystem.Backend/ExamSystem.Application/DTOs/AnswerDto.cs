4f751e8b6c1820478c07f3018f12bb7978555943
Vektor19 <viktor.komissarov@knu.ua>
2025-04-14 20:19:57 +0300

    feat: add Answer dto

---
+++ ExamSystem.Backend/ExamSystem.Application/DTOs/AnswerDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSystem.Application.DTOs
+{
+    public class AnswerDto
+    {
+        public Guid AnswerId { get; set; }
+        public Guid UserId { get; set; }
+        public Guid QuestionOptionId { get; set; }
+    }
+}
