6ab8961c578b3c7260f5c895c1ae6d202bf13c91
Vektor19 <viktor.komissarov@knu.ua>
2025-04-14 20:57:28 +0300

    feat: add iAuthService

---
+++ ExamSystem.Backend/ExamSystem.Application/Interfaces/Services/IAuhService.cs
@@ -0,0 +1,11 @@
+﻿using ExamSystem.Application.DTOs;
+
+namespace ExamSystem.Application.Interfaces.Services
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDto> LoginAsync(LoginUserDto loginDto);
+        Task<AuthResponseDto> RegisterAsync(RegisterUserDto registerDto);
+        Task<bool> ValidateUserAsync(LoginUserDto loginDto);
+    }
+}
