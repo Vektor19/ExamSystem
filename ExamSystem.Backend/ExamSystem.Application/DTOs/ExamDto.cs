778776788454f7e7923c0359a4286b322534d8f0
Vektor19 <viktor.komissarov@knu.ua>
2025-04-14 19:29:22 +0300

    feat: create ExamDto

---
+++ ExamSystem.Backend/ExamSystem.Application/DTOs/ExamDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSystem.Application.DTOs
+{
+    public class ExamDto
+    {
+        public Guid ExamId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public UserDto CreatedBy { get; set; } = null!;
+        public int QuestionCount { get; set; }
+        public int ParticipantCount { get; set; }
+    }
+}
