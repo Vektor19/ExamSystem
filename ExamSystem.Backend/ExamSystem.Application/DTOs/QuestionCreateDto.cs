29bccef5946ca8d218f4d58bebb9650b7cd72930
Vektor19 <viktor.komissarov@knu.ua>
2025-04-14 20:18:52 +0300

    feat: add Quesiton dtos

---
+++ ExamSystem.Backend/ExamSystem.Application/DTOs/QuestionCreateDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSystem.Application.DTOs
+{
+    public class QuestionCreateDto
+    {
+        public string QuestionText { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public List<QuestionOptionCreateDto> Options { get; set; } = [];
+    }
+}
---
+++ ExamSystem.Backend/ExamSystem.Application/DTOs/QuestionDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSystem.Application.DTOs
+{
+    internal class QuestionDto
+    {
+        public Guid QuestionId { get; set; }
+        public string QuestionText { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public List<QuestionOptionDto> Options { get; set; } = [];
+    }
+}
