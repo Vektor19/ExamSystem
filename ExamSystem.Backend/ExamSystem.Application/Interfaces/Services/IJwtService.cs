854793f10787ad6bf70b3478c1384670b205bfea
Vektor19 <viktor.komissarov@knu.ua>
2025-04-14 20:50:41 +0300

    feat: add IJwtService interface

---
+++ ExamSystem.Backend/ExamSystem.Application/Interfaces/Services/IJwtService.cs
@@ -0,0 +1,12 @@
+﻿using ExamSystem.Application.DTOs;
+using System.Security.Claims;
+
+namespace ExamSystem.Application.Interfaces.Services
+{
+    public interface IJwtService
+    {
+        string GenerateToken(UserDto user);
+        ClaimsPrincipal GetPrincipalFromToken(string token);
+        bool ValidateToken(string token);
+    }
+}
