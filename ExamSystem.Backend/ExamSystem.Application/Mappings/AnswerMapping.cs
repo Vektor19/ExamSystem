624ada773406cdc4f781505afcb2ec1b50203d4b
Vektor19 <viktor.komissarov@knu.ua>
2025-04-18 19:06:08 +0300

    feat: add AnswerMapping profile

---
+++ ExamSystem.Backend/ExamSystem.Application/Mappings/AnswerMapping.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using ExamSystem.Application.DTOs;
+using ExamSystem.Core.Entities;
+
+namespace ExamSystem.Application.Mappings
+{
+    public class AnswerMapping : Profile
+    {
+        public AnswerMapping()
+        {
+            CreateMap<Answer, AnswerDto>().ReverseMap();
+        }
+    }
+}
