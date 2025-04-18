674c46f38f583d1d1c6cbe4671963a44667c41ec
Vektor19 <viktor.komissarov@knu.ua>
2025-04-18 19:02:54 +0300

    feat: add ExamMapping profile

---
+++ ExamSystem.Backend/ExamSystem.Application/Mappings/ExamMapping.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using ExamSystem.Application.DTOs;
+using ExamSystem.Core.Entities;
+
+namespace ExamSystem.Application.Mappings
+{
+    public class ExamMapping : Profile
+    {
+        public ExamMapping()
+        {
+            CreateMap<Exam, ExamDto>()
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
+                .ForMember(dest => dest.CreatedBy, opt => opt.MapFrom(src => src.UserCreatedBy))
+                .ForMember(dest => dest.QuestionCount, opt => opt.MapFrom(src => src.Questions.Count))
+                .ForMember(dest => dest.ParticipantCount, opt => opt.MapFrom(src => src.ExamUsers.Count));
+
+            CreateMap<ExamCreateDto, Exam>();
+            CreateMap<ExamUpdateDto, Exam>();
+        }
+    }
+}
