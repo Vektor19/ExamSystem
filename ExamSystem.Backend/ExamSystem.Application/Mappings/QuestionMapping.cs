0cc4dbd14d8f19e05869979fce9f6683308246e6
Vektor19 <viktor.komissarov@knu.ua>
2025-04-18 19:04:16 +0300

    feat: add QuestionMapping profile

---
+++ ExamSystem.Backend/ExamSystem.Application/Mappings/QuestionMapping.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using ExamSystem.Application.DTOs;
+using ExamSystem.Core.Entities;
+using ExamSystem.Core.Enums;
+
+namespace ExamSystem.Application.Mappings
+{
+    public class QuestionMapping : Profile
+    {
+        public QuestionMapping()
+        {
+            CreateMap<Question, QuestionDto>()
+                .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.Type.ToString()))
+                .ForMember(dest => dest.Options, opt => opt.MapFrom(src => src.QuestionOptions));
+
+            CreateMap<QuestionCreateDto, Question>()
+                .ForMember(dest => dest.Type, opt => opt.MapFrom(src => Enum.Parse<QuestionType>(src.Type)))
+                .ForMember(dest => dest.QuestionOptions, opt => opt.MapFrom(src => src.Options));
+        }
+    }
+}
