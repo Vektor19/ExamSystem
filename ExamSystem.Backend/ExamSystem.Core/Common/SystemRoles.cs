5aaac0b57e187c93ec1426340816497681212dc5
Vektor19 <viktor.komissarov@knu.ua>
2025-04-18 22:07:35 +0300

    feat: create SystemRoles static class with const fields which represent system roles names

---
+++ ExamSystem.Backend/ExamSystem.Core/Common/SystemRoles.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamSystem.Core.Common
+{
+    public static class SystemRoles
+    {
+        public const string Admin = "Admin";
+        public const string Student = "Student";
+        public const string Examinator = "Examinator";
+        public static readonly string[] All = [Admin, Student, Examinator];
+    }
+}
