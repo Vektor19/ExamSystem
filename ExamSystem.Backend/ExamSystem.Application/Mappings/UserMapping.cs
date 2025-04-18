db7c9d0196fe1aad3696861ddffca2eeac6abf05
Vektor19 <viktor.komissarov@knu.ua>
2025-04-18 19:07:11 +0300

    feat: add UserMapping profile

---
+++ ExamSystem.Backend/ExamSystem.Application/Mappings/UserMapping.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using ExamSystem.Application.DTOs;
+using ExamSystem.Core.Entities;
+
+namespace ExamSystem.Application.Mappings
+{
+    public class UserMapping : Profile
+    {
+        public UserMapping()
+        {
+            CreateMap<User, UserDto>()
+                .ForMember(dest => dest.Roles, opt => opt.MapFrom(src => src.UserRoles.Select(ur => ur.Role.Name)));
+
+            CreateMap<RegisterUserDto, User>()
+                .ForMember(dest => dest.UserRoles, opt => opt.Ignore())
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore());
+        }
+    }
+}
