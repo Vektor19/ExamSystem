3929ee7b260c482e810eb1c19ec69424736283e0
Vektor19 <viktor.komissarov@knu.ua>
2025-04-18 20:00:12 +0300

    feat: add Password hashing in Infrastructure project

---
+++ ExamSystem.Backend/ExamSystem.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using ExamSystem.Core.Interfaces.Security;
+using ExamSystem.Infrastructure.Security;
+
+namespace ExamSystem.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IPasswordHasher, PasswordHasher>();
+            return services;
+        }
+    }
+}
