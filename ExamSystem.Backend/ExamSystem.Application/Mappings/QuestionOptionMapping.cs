9324a53c51a749aac0fd2cd66c1a145da4f7bc75
Vektor19 <viktor.komissarov@knu.ua>
2025-04-18 19:05:23 +0300

    feat: add QuestionOptionMapping profile

---
+++ ExamSystem.Backend/ExamSystem.Application/Mappings/QuestionOptionMapping.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using ExamSystem.Application.DTOs;
+using ExamSystem.Core.Entities;
+using ExamSystem.Core.Enums;
+
+namespace ExamSystem.Application.Mappings
+{
+    public class QuestionOptionMapping : Profile
+    {
+        public QuestionOptionMapping()
+        {
+            CreateMap<QuestionOption, QuestionOptionDto>();
+            CreateMap<QuestionOptionCreateDto, QuestionOption>();
+        }
+    }
+}
