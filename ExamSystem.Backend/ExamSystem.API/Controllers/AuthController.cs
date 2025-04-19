ad027a79dad87ce47bfcb93413ea181ffa2c6dca
Vektor19 <viktor.komissarov@knu.ua>
2025-04-19 13:38:16 +0300

    feat: create AuthController

---
+++ ExamSystem.Backend/ExamSystem.API/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ExamSystem.Application.DTOs;
+using ExamSystem.Application.Interfaces.Services;
+
+namespace ExamSystem.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginUserDto loginDto)
+        {
+            var result = await _authService.LoginAsync(loginDto);
+            return result.Success ? Ok(result.Data) : Unauthorized(result.ErrorMessage);
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterUserDto registerDto)
+        {
+            var result = await _authService.RegisterAsync(registerDto);
+            return result.Success ? Ok(result.Data) : BadRequest(result.ErrorMessage);
+        }
+    }
+}
