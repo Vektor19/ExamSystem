69c66fcbf18ee1967945b645e6c815dea48651dc
Vektor19 <viktor.komissarov@knu.ua>
2025-04-19 14:32:51 +0300

    feat: add Authorize attributes

--- ExamSystem.Backend/ExamSystem.API/Controllers/UserController.cs
+++ ExamSystem.Backend/ExamSystem.API/Controllers/UserController.cs
@@ -2,10 +2,12 @@
 using ExamSystem.Application.DTOs;
 using ExamSystem.Application.Interfaces.Services;
 using ExamSystem.Core.Common;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ExamSystem.API.Controllers
 {
     [ApiController]
+    [Authorize]
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
@@ -17,35 +19,35 @@ namespace ExamSystem.API.Controllers
             _logger = logger;
             _userService = userService;
         }
-
+        [Authorize(Roles = SystemRoles.Admin)]
         [HttpPost("create-by-admin")]
         public async Task<IActionResult> CreateByAdmin([FromBody] CreateUserByAdminDto userDto)
         {
             var result = await _userService.CreateUserByAdminAsync(userDto);
             return result.Success ? Ok(result) : BadRequest(result.ErrorMessage);
         }
-
+        [Authorize(Roles = SystemRoles.Admin)]
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
             var result = await _userService.GetAllAsync();
             return result.Success ? Ok(result.Data) : NotFound(result.ErrorMessage);
         }
-
+        [SelfOrAdminAuthorize]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(Guid id)
         {
             var result = await _userService.GetByIdAsync(id);
             return result.Success ? Ok(result.Data) : NotFound(result.ErrorMessage);
         }
-
+        [SelfOrAdminAuthorize]
         [HttpPut("{id}")]
         public async Task<IActionResult> Update(Guid id, [FromBody] UpdateUserDto updateDto)
         {
             var result = await _userService.UpdateAsync(id, updateDto);
             return result.Success ? Ok(result) : BadRequest(result.ErrorMessage);
         }
-
+        [SelfOrAdminAuthorize]
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(Guid id)
         {
