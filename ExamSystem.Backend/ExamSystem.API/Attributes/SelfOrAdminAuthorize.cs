c4df091757ab2918a18ed04df1c99b35474abafc
Vektor19 <viktor.komissarov@knu.ua>
2025-04-19 14:32:36 +0300

    feat: add SelfOrAdminAuthorize attribute to give access for authorized user get its data or it can be an admin

---
+++ ExamSystem.Backend/ExamSystem.API/Attributes/SelfOrAdminAuthorize.cs
@@ -0,0 +1,38 @@
+﻿using ExamSystem.Core.Common;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+
+public class SelfOrAdminAuthorize : Attribute, IAuthorizationFilter
+{
+    public string RouteKey { get; set; } = "id";
+
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var user = context.HttpContext.User;
+        var userRole = user.FindFirstValue(ClaimTypes.Role);
+        var userId = user.FindFirstValue(JwtRegisteredClaimNames.Sub);
+
+        if (userId == null)
+        {
+            context.Result = new UnauthorizedResult();
+            return;
+        }
+
+        if (userRole == SystemRoles.Admin)
+            return;
+
+        var routeIdString = context.RouteData.Values[RouteKey]?.ToString();
+        if (!Guid.TryParse(routeIdString, out var routeId))
+        {
+            context.Result = new ForbidResult();
+            return;
+        }
+
+        if (userId != routeId.ToString())
+        {
+            context.Result = new ForbidResult();
+        }
+    }
+}
