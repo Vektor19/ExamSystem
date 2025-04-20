5d0aad69467d0778e56b6a3165e71019ff72c0b5
Vektor19 <viktor.komissarov@knu.ua>
2025-04-20 13:51:52 +0300

    feat: create IExamService interface to define service operations for managing exams, including methods for creating, retrieving, updating, and deleting exams

---
+++ ExamSystem.Backend/ExamSystem.Application/Interfaces/Services/IExamService.cs
@@ -0,0 +1,16 @@
+﻿using ExamSystem.Application.DTOs;
+using ExamSystem.Core.Common;
+
+namespace ExamSystem.Application.Interfaces.Services
+{
+    public interface IExamService
+    {
+        Task<OperationResult<ExamDto>> GetByIdAsync(Guid examId);
+        Task<OperationResult<IEnumerable<ExamDto>>> GetAllAsync();
+        Task<OperationResult<IEnumerable<ExamDto>>> GetAllByCreatedUserIdAsync();
+        Task<OperationResult<IEnumerable<ExamDto>>> GetAllByParticipantUserIdAsync();
+        Task<OperationResult> UpdateAsync(Guid examId, ExamUpdateDto examUpdateDto);
+        Task<OperationResult> DeleteAsync(Guid examId);
+        Task<OperationResult> CreateAsync(ExamCreateDto createExamDto);
+    }
+}
