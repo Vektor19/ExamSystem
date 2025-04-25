e88097f875effc59bbef79386a531abdf5854428
Vektor19 <viktor.komissarov@knu.ua>
2025-04-25 12:40:52 +0300

    feat: create migration ExamAnswerRelation

---
+++ ExamSystem.Backend/ExamSystem.Persistence/Migrations/20250425094003_ExamAnswerRelation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExamSystem.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExamAnswerRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ExamId",
+                table: "Answers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answers_ExamId",
+                table: "Answers",
+                column: "ExamId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Answers_Exams_ExamId",
+                table: "Answers",
+                column: "ExamId",
+                principalTable: "Exams",
+                principalColumn: "ExamId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Answers_Exams_ExamId",
+                table: "Answers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Answers_ExamId",
+                table: "Answers");
+
+            migrationBuilder.DropColumn(
+                name: "ExamId",
+                table: "Answers");
+        }
+    }
+}
