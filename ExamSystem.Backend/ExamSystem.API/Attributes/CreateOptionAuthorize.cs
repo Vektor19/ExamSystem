e2820cf3754aafd74d3d2a383c586d44e48c881d
Vektor19 <viktor.komissarov@knu.ua>
2025-04-25 17:10:38 +0300

    feat: add QuesitonOption controller with api methods

---
+++ ExamSystem.Backend/ExamSystem.API/Attributes/CreateOptionAuthorize.cs
@@ -0,0 +1,74 @@
+﻿using ExamSystem.Application.Interfaces.Services;
+using ExamSystem.Core.Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+using System.Text.Json;
+
+public class CreateOptionAuthorize : Attribute, IAsyncAuthorizationFilter
+{
+    private readonly IQuestionService _questionService;
+
+    public CreateOptionAuthorize(IQuestionService questionService)
+    {
+        _questionService = questionService;
+    }
+    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        var user = context.HttpContext.User;
+        var userRole = user.FindFirstValue(ClaimTypes.Role);
+        var userId = user.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(userRole))
+        {
+            context.Result = new UnauthorizedResult();
+            return;
+        }
+
+        if (userRole != SystemRoles.Admin && userRole != SystemRoles.Examinator)
+        {
+            context.Result = new ForbidResult();
+            return;
+        }
+
+        context.HttpContext.Request.EnableBuffering();
+        using var reader = new StreamReader(context.HttpContext.Request.Body, leaveOpen: true);
+        var body = await reader.ReadToEndAsync();
+        context.HttpContext.Request.Body.Position = 0;
+
+        try
+        {
+            using var jsonDoc = JsonDocument.Parse(body);
+            if (jsonDoc.RootElement.TryGetProperty("questionId", out var quesitonProp))
+            {
+                var questionIdString = quesitonProp.GetString();
+                if (!Guid.TryParse(questionIdString, out var questionId))
+                {
+                    context.Result = new ForbidResult();
+                    return;
+                }
+
+                var resultQuestion = await _questionService.GetByIdAsync(questionId);
+                if (!resultQuestion.Success || resultQuestion.Data == null)
+                {
+                    context.Result = new ForbidResult();
+                    return;
+                }
+                var question = resultQuestion.Data!;
+
+                if (question.Exam.CreatedBy.UserId.ToString() != userId)
+                {
+                    context.Result = new ForbidResult();
+                }
+            }
+            else
+            {
+                context.Result = new BadRequestObjectResult("Field 'questionId' is required.");
+            }
+        }
+        catch (JsonException)
+        {
+            context.Result = new BadRequestObjectResult("Invalid JSON.");
+        }
+    }
+}
---
+++ ExamSystem.Backend/ExamSystem.API/Controllers/QuestionOptionController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ExamSystem.Application.DTOs;
+using ExamSystem.Application.Interfaces.Services;
+using ExamSystem.Core.Common;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ExamSystem.API.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("[controller]")]
+    public class QuestionOptionController : ControllerBase
+    {
+        private readonly ILogger<QuestionOptionController> _logger;
+        private readonly IQuestionOptionService _questionOptionService;
+
+        public QuestionOptionController(ILogger<QuestionOptionController> logger, IQuestionOptionService questionService)
+        {
+            _logger = logger;
+            _questionOptionService = questionService;
+        }
+        [Authorize(Roles = SystemRoles.Admin)]
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _questionOptionService.GetAllAsync();
+            return result.Success ? Ok(result.Data) : NotFound(result.ErrorMessage);
+        }
+        [TypeFilter(typeof(QuestionOwnerOrAdminAuthorize))]
+        [HttpGet("by-question/{id}")]
+        public async Task<IActionResult> GetAllByQuestion(Guid id)
+        {
+            var result = await _questionOptionService.GetAllByQuestionIdAsync(id);
+            return result.Success ? Ok(result.Data) : NotFound(result.ErrorMessage);
+        }
+
+        [TypeFilter(typeof(OptionOwnerOrAdminAuthorize))]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var result = await _questionOptionService.GetByIdAsync(id);
+            return result.Success ? Ok(result.Data) : NotFound(result.ErrorMessage);
+        }
+        [TypeFilter(typeof(CreateOptionAuthorize))]
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] QuestionOptionCreateDto questionDto)
+        {
+            var result = await _questionOptionService.CreateAsync(questionDto);
+            return result.Success ? Ok(result) : BadRequest(result.ErrorMessage);
+        }
+        [TypeFilter(typeof(OptionOwnerOrAdminAuthorize))]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var result = await _questionOptionService.DeleteAsync(id);
+            return result.Success ? Ok(result) : BadRequest(result.ErrorMessage);
+        }
+        [TypeFilter(typeof(OptionOwnerOrAdminAuthorize))]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] QuestionOptionUpdateDto questionDto)
+        {
+            var result = await _questionOptionService.UpdateAsync(id, questionDto);
+            return result.Success ? Ok(result) : BadRequest(result.ErrorMessage);
+        }
+    }
+}
