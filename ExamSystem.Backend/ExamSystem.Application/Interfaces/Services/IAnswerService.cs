793f45b1a7b9adf0c4fbaf878eb92203b9d42a4c
Vektor19 <viktor.komissarov@knu.ua>
2025-04-25 12:06:17 +0300

    feat: create AnswerService with implemented methods

---
+++ ExamSystem.Backend/ExamSystem.Application/Interfaces/Services/IAnswerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using ExamSystem.Application.DTOs;
+using ExamSystem.Core.Common;
+
+namespace ExamSystem.Application.Interfaces.Services
+{
+    public interface IAnswerService
+    {
+        Task<OperationResult<AnswerDto>> GetByIdAsync(Guid answerId);
+        Task<OperationResult<IEnumerable<AnswerDto>>> GetAllAsync();
+        Task<OperationResult<IEnumerable<AnswerDto>>> GetAllByExamIdAsync(Guid examId);
+        Task<OperationResult<IEnumerable<AnswerDto>>> GetAllByUserIdAsync(Guid userId);
+        Task<OperationResult> DeleteAsync(Guid answerId);
+        Task<OperationResult> CreateAsync(AnswerDto createAnswerDto);
+    }
+}
---
+++ ExamSystem.Backend/ExamSystem.Application/Services/AnswerService.cs
@@ -0,0 +1,88 @@
+﻿using AutoMapper;
+using ExamSystem.Application.DTOs;
+using ExamSystem.Application.Interfaces.Services;
+using ExamSystem.Core.Common;
+using ExamSystem.Core.Entities;
+using ExamSystem.Core.Enums;
+using ExamSystem.Core.Interfaces.Repositories;
+
+namespace ExamSystem.Application.Services
+{
+    public class AnswerService : IAnswerService
+    {
+        private readonly IAnswerRepository _answerRepository;
+        private readonly IMapper _mapper;
+        public AnswerService(IMapper mapper, IAnswerRepository answerRepository)
+        {
+            _answerRepository = answerRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<OperationResult> CreateAsync(AnswerDto createAnswerDto)
+        {
+            var answer = _mapper.Map<Answer>(createAnswerDto);
+            answer.AnswerId = Guid.NewGuid();
+
+            var result = await _answerRepository.AddAsync(answer);
+            return result.Success
+                ? OperationResult.Ok()
+                : OperationResult.Fail("Failed to create answer.");
+        }
+
+
+        public async Task<OperationResult> DeleteAsync(Guid id)
+        {
+            var result = await _answerRepository.DeleteAsync(id);
+            return result.Success
+                ? OperationResult.Ok()
+                : OperationResult.Fail("Failed to delete answer.");
+        }
+
+        public async Task<OperationResult<IEnumerable<AnswerDto>>> GetAllAsync()
+        {
+            var result = await _answerRepository.GetAllAsync();
+            if (!result.Success)
+                return OperationResult<IEnumerable<AnswerDto>>.Fail(result.ErrorMessage!);
+            if (!result.Data!.Any())
+                return OperationResult<IEnumerable<AnswerDto>>.Fail("No answers found.");
+
+            var examDtos = _mapper.Map<IEnumerable<AnswerDto>>(result.Data);
+            return OperationResult<IEnumerable<AnswerDto>>.Ok(examDtos);
+        }
+
+        public async Task<OperationResult<IEnumerable<AnswerDto>>> GetAllByExamIdAsync(Guid examId)
+        {
+            var result = await _answerRepository.GetAllAsync();
+            if (!result.Success)
+                return OperationResult<IEnumerable<AnswerDto>>.Fail(result.ErrorMessage!);
+            var answers = result.Data!;
+            var filteredAnswers = answers.Where(e => e.ExamId == examId).ToList();
+            if (!filteredAnswers.Any())
+                return OperationResult<IEnumerable<AnswerDto>>.Fail("No answers found for this exam.");
+            var answerDtos = _mapper.Map<IEnumerable<AnswerDto>>(filteredAnswers);
+            return OperationResult<IEnumerable<AnswerDto>>.Ok(answerDtos);
+        }
+        public async Task<OperationResult<IEnumerable<AnswerDto>>> GetAllByUserIdAsync(Guid userId)
+        {
+            var result = await _answerRepository.GetAllAsync();
+            if (!result.Success)
+                return OperationResult<IEnumerable<AnswerDto>>.Fail(result.ErrorMessage!);
+            var answers = result.Data!;
+            var filteredAnswers = answers.Where(e => e.UserId == userId).ToList();
+            if (!filteredAnswers.Any())
+                return OperationResult<IEnumerable<AnswerDto>>.Fail("No answers found for this user.");
+            var answerDtos = _mapper.Map<IEnumerable<AnswerDto>>(filteredAnswers);
+            return OperationResult<IEnumerable<AnswerDto>>.Ok(answerDtos);
+        }
+
+        public async Task<OperationResult<AnswerDto>> GetByIdAsync(Guid id)
+        {
+            var result = await _answerRepository.GetByIdAsync(id);
+            if (!result.Success)
+                return OperationResult<AnswerDto>.Fail(result.ErrorMessage!);
+
+            var answerDto = _mapper.Map<AnswerDto>(result.Data);
+            return OperationResult<AnswerDto>.Ok(answerDto);
+        }
+    }
+}
