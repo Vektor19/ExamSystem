52a36f0256d5f8defe1eeb3cf62756705b3026f3
Vektor19 <viktor.komissarov@knu.ua>
2025-04-29 01:29:15 +0300

    feat: enhance Home Page UI and add new dependencies
    - Added Material-UI components for improved styling and functionality.
    - Introduced new images for the home page and main page.
    - Updated Home component to include a button for navigation and improved layout.
    - Enhanced Home.module.css with new styles for better visual presentation.
    - Modified HomeHeader and PublicOutlet styles for consistent design.
    - Adjusted global CSS variables for header height.

--- ExamSystem.Frontend/src/Components/Home/Home.tsx
+++ ExamSystem.Frontend/src/Components/Home/Home.tsx
@@ -1,13 +1,35 @@
 import homeStyles from "../../Styles/Home.module.css";
 import outletStyles from "../../Styles/PublicOutlet.module.css";
+import mainLogo from "/home_page.png";
+import { Button } from "@mui/material";
 
 const Home = () => {
   return (
     <>
       <section className={`${outletStyles["public-outlet"]}`}>
         <div className={`${homeStyles["home-page-container"]} `}>
-          <h1>Welcome to the Exam System</h1>
-          <p>This is the home page of our application.</p>
+          <div className={homeStyles["home-page-introduction"]}>
+            <div className={homeStyles["home-page-introduction-text"]}>
+              <h1>Welcome to the Exam System</h1>
+              <h3>This is the home page of our application.</h3>
+              <Button
+                variant="contained"
+                color="primary"
+                className={homeStyles["home-page-button"]}
+                onClick={() => {
+                  window.location.href = "/dashboard";
+                }}
+              >
+                Get Started
+              </Button>
+            </div>
+
+            <img
+              src={mainLogo}
+              alt="Main Page"
+              className={homeStyles["main-page-logo"]}
+            />
+          </div>
         </div>
       </section>
     </>
