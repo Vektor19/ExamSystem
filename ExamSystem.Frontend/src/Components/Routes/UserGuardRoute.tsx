61468d9e60b6b50009ca2138bd00818d22743e6a
Vektor19 <viktor.komissarov@knu.ua>
2025-05-01 17:47:21 +0300

    feat: enhance UserGuardRoute to fetch user data on mount and handle loading state

--- ExamSystem.Frontend/src/Components/Routes/UserGuardRoute.tsx
+++ ExamSystem.Frontend/src/Components/Routes/UserGuardRoute.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { useUser } from "../../Providers/UserProvider";
 import LoadingPage from "../Extra/LoadingPage";
 
 const UserGuardRoute = ({ children }: { children: React.ReactNode }) => {
-  const { user, loading } = useUser();
-
+  const { user, loading, fetchUser } = useUser();
+  useEffect(() => {
+    if (!user && !loading) {
+      fetchUser();
+    }
+  }, [user, loading]);
   if (loading) return <LoadingPage />;
   return user ? <>{children}</> : <Navigate to="/login" />;
 };
