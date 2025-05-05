f6002a32da373010282308f7e3d7ab03cc6da039
Vektor19 <viktor.komissarov@knu.ua>
2025-05-05 12:49:46 +0300

    feat: add Exam interface for exam management

---
+++ ExamSystem.Frontend/src/Models/Exam.ts
@@ -0,0 +1,13 @@
+import { User } from "./User";
+
+export interface Exam {
+  examId: string;
+  name: string;
+  createdDate: string;
+  startDate: string;
+  endDate: string;
+  status: string;
+  createdBy: User;
+  questionCount: number;
+  participantCount: number;
+}
