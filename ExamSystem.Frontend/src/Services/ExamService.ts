8c103393d9a3f2e44a075848756b7fa52a81558c
Vektor19 <viktor.komissarov@knu.ua>
2025-05-05 22:04:21 +0300

    feat: add updateExam method to ExamService for updating exam details

--- ExamSystem.Frontend/src/Services/ExamService.ts
+++ ExamSystem.Frontend/src/Services/ExamService.ts
@@ -1,6 +1,7 @@
 import examSystemApi from "../Api/examSystemApi";
 import { Exam } from "../Models/Exam";
 import { ExamCreate } from "../Models/ExamCreate";
+import { ExamUpdate } from "../Models/ExamUpdate";
 
 class ExamService {
   async getExamById(id: string): Promise<Exam> {
@@ -33,6 +34,14 @@ class ExamService {
       return res.data;
     } catch (err: any) {
       throw new Error(err?.response?.data?.message || "Exams not found");
+    }
+  }
+  async updateExam(id: string, exam: ExamUpdate): Promise<boolean> {
+    try {
+      const res = await examSystemApi.put("/exam/" + id, exam);
+      return res.data;
+    } catch (err: any) {
+      throw new Error(err?.response?.data?.message || "Exam not updated");
     }
   }
 }
