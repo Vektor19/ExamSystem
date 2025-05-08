a4095cbd764177c3f9ea7c9f17374685e9661628
Vektor19 <viktor.komissarov@knu.ua>
2025-05-09 02:19:26 +0300

    feat: implement AnswerController and create custom answer authorize

---
+++ ExamSystem.Backend/ExamSystem.API/Attributes/CreateAnswerAuthorize.cs
@@ -0,0 +1,77 @@
+﻿using ExamSystem.Application.Interfaces.Services;
+using ExamSystem.Core.Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+using System.Text.Json;
+
+public class CreateAnswerAuthorize : Attribute, IAsyncAuthorizationFilter
+{
+    private readonly IExamService _examService;
+
+    public CreateAnswerAuthorize(IExamService examService)
+    {
+        _examService = examService;
+    }
+
+    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        var user = context.HttpContext.User;
+        var userId = user.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (string.IsNullOrEmpty(userId))
+        {
+            context.Result = new UnauthorizedResult();
+            return;
+        }
+        var roles = user.FindAll(ClaimTypes.Role).Select(c => c.Value).ToList();
+
+        if (!roles.Contains(SystemRoles.Admin) && !roles.Contains(SystemRoles.Examinator))
+        {
+            context.Result = new ForbidResult();
+            return;
+        }
+
+        context.HttpContext.Request.EnableBuffering();
+        using var reader = new StreamReader(context.HttpContext.Request.Body, leaveOpen: true);
+        var body = await reader.ReadToEndAsync();
+        context.HttpContext.Request.Body.Position = 0;
+
+        try
+        {
+            using var jsonDoc = JsonDocument.Parse(body);
+            if (jsonDoc.RootElement.TryGetProperty("userId", out var userIdProp))
+            {
+                var userIdFromAnswer = userIdProp.GetString();
+                if (userIdFromAnswer != userId)
+                {
+                    context.Result = new ForbidResult();
+                }
+                if (jsonDoc.RootElement.TryGetProperty("examId", out var examIdProp))
+                {
+                    var examId = examIdProp.GetGuid();
+                    if ((await _examService.IsParticipantAsync(examId, userIdProp.GetGuid())).Success)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        context.Result = new ForbidResult();
+                    }
+                }
+                else
+                {
+                    context.Result = new BadRequestObjectResult("Field 'examId' is required.");
+                }
+            }
+            else
+            {
+                context.Result = new BadRequestObjectResult("Field 'userId' is required.");
+            }
+        }
+        catch (JsonException)
+        {
+            context.Result = new BadRequestObjectResult("Invalid JSON.");
+        }
+    }
+}
---
+++ ExamSystem.Backend/ExamSystem.API/Controllers/AnswerController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ExamSystem.Application.DTOs;
+using ExamSystem.Application.Interfaces.Services;
+using ExamSystem.Core.Common;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ExamSystem.API.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("[controller]")]
+    public class AnswerController : ControllerBase
+    {
+        private readonly ILogger<AnswerController> _logger;
+        private readonly IAnswerService _answerService;
+
+        public AnswerController(ILogger<AnswerController> logger, IAnswerService examService)
+        {
+            _logger = logger;
+            _answerService = examService;
+        }
+        [Authorize(Roles = SystemRoles.Admin)]
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _answerService.GetAllAsync();
+            return result.Success ? Ok(result.Data) : NotFound(result.ErrorMessage);
+        }
+        [TypeFilter(typeof(ExamOwnerOrAdminAuthorize))]
+        [HttpGet("by-exam/{id}")]
+        public async Task<IActionResult> GetAllByExamId(Guid id)
+        {
+            var result = await _answerService.GetAllByExamIdAsync(id);
+            return result.Success ? Ok(result.Data) : NotFound(result.ErrorMessage);
+        }
+
+        [TypeFilter(typeof(SelfOrAdminAuthorize))]
+        [HttpGet("by-participant/{id}")]
+        public async Task<IActionResult> GetAllByParticipantIdAsync(Guid id)
+        {
+            var result = await _answerService.GetAllByUserIdAsync(id);
+            return result.Success ? Ok(result.Data) : NotFound(result.ErrorMessage);
+        }
+
+        [Authorize(Roles = SystemRoles.Admin)]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var result = await _answerService.GetByIdAsync(id);
+            return result.Success ? Ok(result.Data) : NotFound(result.ErrorMessage);
+        }
+        [TypeFilter(typeof(CreateAnswerAuthorize))]
+        [HttpPost("open-type")]
+        public async Task<IActionResult> CreateOpenAnswer([FromBody] CreateOpenAnswerDto answerDto)
+        {
+            var result = await _answerService.CreateOpenAnswerAsync(answerDto);
+            return result.Success ? Ok(result) : BadRequest(result.ErrorMessage);
+        }
+
+        [TypeFilter(typeof(CreateAnswerAuthorize))]
+        [HttpPost("option-type")]
+        public async Task<IActionResult> CreateOptionAnswer([FromBody] CreateOptionAnswerDto answerDto)
+        {
+            var result = await _answerService.CreateOptionAnswerAsync(answerDto);
+            return result.Success ? Ok(result) : BadRequest(result.ErrorMessage);
+        }
+    }
+}
