db8e2512a3ff0c2296591e4b8fcfca2d93ed3804
Vektor19 <viktor.komissarov@knu.ua>
2025-05-08 14:01:21 +0300

    feat: add static class ExamValidator with exam status validaton and dates

---
+++ ExamSystem.Backend/ExamSystem.Application/Utils/Validators/ExamValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExamSystem.Core.Common;
+using ExamSystem.Core.Entities;
+using ExamSystem.Core.Enums;
+
+namespace ExamSystem.Application.Utils.Validators
+{
+    public static class ExamValidator
+    {
+        public static OperationResult ValidateNotStarted(Exam exam)
+        {
+            return exam.Status == ExamStatus.NotStarted
+                ? OperationResult.Ok()
+                : OperationResult.Fail("Operation allowed only when exam status is NotStarted.");
+        }
+        public static OperationResult ValidateDates(Exam exam)
+        {
+            if (exam.StartDate < DateTime.UtcNow)
+                return OperationResult.Fail("Start date must be in the future.");
+            if (exam.EndDate <= exam.StartDate)
+                return OperationResult.Fail("End date must be after start date.");
+            return OperationResult.Ok();
+        }
+    }
+}
