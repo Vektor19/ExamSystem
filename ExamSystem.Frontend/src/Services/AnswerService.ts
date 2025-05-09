23449a372c2827b0f07e88e7c74778ad71ab6a2e
Vektor19 <viktor.komissarov@knu.ua>
2025-05-09 20:16:19 +0300

    feat: implement AnswerService with methods to create open and option answers

---
+++ ExamSystem.Frontend/src/Services/AnswerService.ts
@@ -0,0 +1,24 @@
+import examSystemApi from "../Api/examSystemApi";
+import { OpenAnswerCreate, OptionAnswerCreate } from "../Models/AnswerCreate";
+
+class AnswerService {
+  async createOpenAnswer(answer: OpenAnswerCreate): Promise<boolean> {
+    try {
+      const res = await examSystemApi.post("/answer/open-type", answer);
+      return res.data.success;
+    } catch (err: any) {
+      throw new Error(err?.response?.data?.message || "Answer not created");
+    }
+  }
+  
+  async createOptionAnswer(answer: OptionAnswerCreate): Promise<boolean> {
+    try {
+      const res = await examSystemApi.post("/answer/open-type", answer);
+      return res.data.success;
+    } catch (err: any) {
+      throw new Error(err?.response?.data?.message || "Answer not created");
+    }
+  }
+}
+
+export default new AnswerService();
