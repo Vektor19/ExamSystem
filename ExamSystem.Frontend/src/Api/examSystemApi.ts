d1caefb6d39ca99891bcff5a87c6bb7c9a6b4694
Vektor19 <viktor.komissarov@knu.ua>
2025-05-10 21:24:32 +0300

    fix: update baseURL in examSystemApi to point to the correct server address

--- ExamSystem.Frontend/src/Api/examSystemApi.ts
+++ ExamSystem.Frontend/src/Api/examSystemApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const examSystemApi = axios.create({
-  baseURL: 'https://localhost:7197',
+  baseURL: 'http://192.168.5.102:7777',
   headers: {
     'Content-Type': 'application/json',
   },
