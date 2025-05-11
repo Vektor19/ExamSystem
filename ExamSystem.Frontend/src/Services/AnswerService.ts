eca4eccdc7621c64c95243c379dd15632c2a1831
Vektor19 <viktor.komissarov@knu.ua>
2025-05-11 17:28:07 +0300

    feat: add getAllByExamUserId method to AnswerService for fetching answers by exam user ID

--- ExamSystem.Frontend/src/Services/AnswerService.ts
+++ ExamSystem.Frontend/src/Services/AnswerService.ts
@@ -1,4 +1,5 @@
 import examSystemApi from "../Api/examSystemApi";
+import { Answer } from "../Models/Answer";
 import { OpenAnswerCreate, OptionAnswerCreate } from "../Models/AnswerCreate";
 
 class AnswerService {
@@ -10,13 +11,21 @@ class AnswerService {
       throw new Error(err?.response?.data?.message || "Answer not created");
     }
   }
-  
+
   async createOptionAnswer(answer: OptionAnswerCreate): Promise<boolean> {
     try {
       const res = await examSystemApi.post("/answer/option-type", answer);
       return res.data.success;
     } catch (err: any) {
       throw new Error(err?.response?.data?.message || "Answer not created");
+    }
+  }
+  async getAllByExamUserId(examUserId: string): Promise<Answer[]> {
+    try {
+      const res = await examSystemApi.get("/answer/by-examuser/" + examUserId);
+      return res.data;
+    } catch (err: any) {
+      throw new Error(err?.response?.data?.message || "Answers not found");
     }
   }
 }
