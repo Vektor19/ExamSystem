79aa61d631c437e3c933e0c5ca2222b9d2c56fa6
Vektor19 <viktor.komissarov@knu.ua>
2025-05-14 13:22:48 +0300

    feat: add LoginUserDtoValidator for user login validation

---
+++ ExamSystem.Backend/ExamSystem.Application/DTOs/User/LoginUserDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace ExamSystem.Application.DTOs.User
+{
+    public class LoginUserDtoValidator : AbstractValidator<LoginUserDto>
+    {
+        public LoginUserDtoValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email is required.")
+                .EmailAddress()
+                .WithMessage("Invalid email format.");
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("Password is required.")
+                .MinimumLength(8)
+                .WithMessage("Password must be at least 8 characters long.");
+        }
+    }
+}
