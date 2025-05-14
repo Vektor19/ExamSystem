e194f97a599706f7a85301489b1ce56c0bd54670
Vektor19 <viktor.komissarov@knu.ua>
2025-05-14 13:24:56 +0300

    feat: add RegisterUserDtoValidator for user registration validation

---
+++ ExamSystem.Backend/ExamSystem.Application/DTOs/User/RegisterUserDtoValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+
+namespace ExamSystem.Application.DTOs.User
+{
+    public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>
+    {
+        public RegisterUserDtoValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .WithMessage("First name is required.")
+                .Length(2, 50)
+                .WithMessage("First name must be between 2 and 50 characters.");
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .WithMessage("Last name is required.")
+                .Length(2, 50)
+                .WithMessage("Last name must be between 2 and 50 characters.");
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email is required.")
+                .EmailAddress()
+                .WithMessage("Invalid email format.");
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("Password is required.")
+                .MinimumLength(8)
+                .WithMessage("Password must be at least 8 characters long.");
+        }
+    }
+}
