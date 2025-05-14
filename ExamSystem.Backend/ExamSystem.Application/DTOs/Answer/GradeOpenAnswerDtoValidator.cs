d271fa59e6d7162d501f08af35caebf93fa85071
Vektor19 <viktor.komissarov@knu.ua>
2025-05-14 13:50:44 +0300

    feat: add GradeOpenAnswerDtoValidator for grading open answers

---
+++ ExamSystem.Backend/ExamSystem.Application/DTOs/Answer/GradeOpenAnswerDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using ExamSystem.Application.DTOs.Exam;
+using FluentValidation;
+
+namespace ExamSystem.Application.DTOs.User
+{
+    public class GradeOpenAnswerDtoValidator : AbstractValidator<GradeOpenAnswerDto>
+    {
+        public GradeOpenAnswerDtoValidator()
+        {
+            RuleFor(x => x.ExamUserId)
+                .NotEmpty()
+                .WithMessage("ExamUserId is required.");
+            RuleFor(x => x.AnswerId)
+                .NotEmpty()
+                .WithMessage("AnswerId is required.");
+            RuleFor(x => x.Grade)
+                .NotEmpty()
+                .WithMessage("Grade is required.");
+        }
+    }
+}
