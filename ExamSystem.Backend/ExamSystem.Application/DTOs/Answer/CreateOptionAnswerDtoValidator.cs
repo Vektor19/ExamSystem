61a3527495207ad9c66ffd785ad6a8a15b6a3ea9
Vektor19 <viktor.komissarov@knu.ua>
2025-05-14 13:49:38 +0300

    feat: add CreateOptionAnswerDtoValidator for option answer validation

---
+++ ExamSystem.Backend/ExamSystem.Application/DTOs/Answer/CreateOptionAnswerDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using ExamSystem.Application.DTOs.Exam;
+using FluentValidation;
+
+namespace ExamSystem.Application.DTOs.User
+{
+    public class CreateOptionAnswerDtoValidator : AbstractValidator<CreateOptionAnswerDto>
+    {
+        public CreateOptionAnswerDtoValidator()
+        {
+            RuleFor(x => x.UserId)
+                .NotEmpty()
+                .WithMessage("UserId is required.");
+            RuleFor(x => x.ExamId)
+                .NotEmpty()
+                .WithMessage("ExamId is required.");
+            RuleFor(x => x.QuestionId)
+                .NotEmpty()
+                .WithMessage("QuestionId is required.");
+            RuleFor(x => x.QuestionOptionId)
+                .NotEmpty()
+                .WithMessage("QuestionOptionId is required.");
+        }
+    }
+}
