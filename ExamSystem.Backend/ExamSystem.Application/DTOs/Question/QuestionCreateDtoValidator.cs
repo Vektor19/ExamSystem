4a217f47bf64b4b101322728951bfbdfa49b4885
Vektor19 <viktor.komissarov@knu.ua>
2025-05-14 13:52:33 +0300

    feat: add QuestionCreateDtoValidator for validating question creation

---
+++ ExamSystem.Backend/ExamSystem.Application/DTOs/Question/QuestionCreateDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using ExamSystem.Application.DTOs.Exam;
+using ExamSystem.Application.DTOs.Question;
+using FluentValidation;
+
+namespace ExamSystem.Application.DTOs.User
+{
+    public class QuestionCreateDtoValidator : AbstractValidator<QuestionCreateDto>
+    {
+        public QuestionCreateDtoValidator()
+        {
+            RuleFor(x => x.ExamId)
+                .NotEmpty()
+                .WithMessage("ExamId is required.");
+            RuleFor(x => x.QuestionText)
+                .NotEmpty()
+                .WithMessage("QuestionText is required.");
+            RuleFor(x => x.Type)
+                .IsInEnum()
+                .WithMessage("Type must be either MultiChoice or Text.");
+            RuleFor(x => x.MaxPoints)
+                .GreaterThan(0)
+                .WithMessage("MaxPoints must be greater than 0.");
+        }
+    }
+}
