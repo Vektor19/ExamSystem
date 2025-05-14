1cc6e0480a4ddbe8b8814e686be22658e1081c87
Vektor19 <viktor.komissarov@knu.ua>
2025-05-14 13:44:56 +0300

    feat: add ExamCreateDtoValidator for exam creation validation

---
+++ ExamSystem.Backend/ExamSystem.Application/DTOs/Exam/ExamCreateDtoValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+
+namespace ExamSystem.Application.DTOs.User
+{
+    public class ExamCreateDtoValidator : AbstractValidator<ExamCreateDto>
+    {
+        public ExamCreateDtoValidator()
+        {
+            RuleFor(x => x.CreatedByUserId)
+                .NotEmpty()
+                .WithMessage("CreatedByUserId is required.");
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Name is required.")
+                .MaximumLength(100)
+                .WithMessage("Name must not exceed 100 characters.");
+            RuleFor(x => x.StartDate)
+                .NotEmpty()
+                .WithMessage("StartDate is required.")
+                .Must(startDate => startDate > DateTime.UtcNow)
+                .WithMessage("StartDate must be in the future.");
+            RuleFor(x => x.EndDate)
+                .NotEmpty()
+                .WithMessage("EndDate is required.")
+                .Must((dto, endDate) => endDate > dto.StartDate)
+                .WithMessage("EndDate must be after StartDate.");
+        }
+    }
+}
