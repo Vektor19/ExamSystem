e3608627f0b42ea32b79dc6c5e7815c80585e35e
Vektor19 <viktor.komissarov@knu.ua>
2025-05-14 13:47:30 +0300

    feat: add AddParticipantByEmailDtoValidator for participant email validation

---
+++ ExamSystem.Backend/ExamSystem.Application/DTOs/Exam/AddParticipantByEmailDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using ExamSystem.Application.DTOs.Exam;
+using FluentValidation;
+
+namespace ExamSystem.Application.DTOs.User
+{
+    public class AddParticipantByEmailDtoValidator : AbstractValidator<AddParticipantByEmailDto>
+    {
+        public AddParticipantByEmailDtoValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email is required.")
+                .EmailAddress()
+                .WithMessage("Invalid email format.");
+        }
+    }
+}
