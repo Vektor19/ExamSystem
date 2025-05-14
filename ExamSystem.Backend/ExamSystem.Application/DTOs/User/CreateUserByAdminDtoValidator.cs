d16ef42c5e884399dfa7f77d8296582c41ac365c
Vektor19 <viktor.komissarov@knu.ua>
2025-05-14 13:27:19 +0300

    feat: add CreateUserByAdminDtoValidator for admin user creation validation

---
+++ ExamSystem.Backend/ExamSystem.Application/DTOs/User/CreateUserByAdminDtoValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+
+namespace ExamSystem.Application.DTOs.User
+{
+    public class CreateUserByAdminDtoValidator : AbstractValidator<CreateUserByAdminDto>
+    {
+        public CreateUserByAdminDtoValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .WithMessage("First name is required.")
+                .Length(2, 50)
+                .WithMessage("First name must be between 2 and 50 characters.");
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .WithMessage("Last name is required.")
+                .Length(2, 50)
+                .WithMessage("Last name must be between 2 and 50 characters.");
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email is required.")
+                .EmailAddress()
+                .WithMessage("Invalid email format.");
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("Password is required.")
+                .MinimumLength(8)
+                .WithMessage("Password must be at least 8 characters long.");
+            RuleFor(x => x.Roles)
+                .NotEmpty()
+                .WithMessage("At least one role is required.")
+                .Must(roles => roles.Count > 0)
+                .WithMessage("At least one role must be specified.");
+        }
+    }
+}
