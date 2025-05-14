d7a128a4a3724037cba9f8a9551f5ab810dd245b
Vektor19 <viktor.komissarov@knu.ua>
2025-05-14 23:21:55 +0300

    refactor: add CreateViolationDtoValidator for validating violation creation requests

---
+++ ExamSystem.Backend/ExamSystem.Application/DTOs/Violation/CreateViolationDtoValidator.cs
@@ -0,0 +1,26 @@
+﻿using ExamSystem.Application.DTOs.Violation;
+using ExamSystem.Core.Enums;
+using FluentValidation;
+
+namespace ExamSystem.Application.DTOs.User
+{
+    public class CreateViolationDtoValidator : AbstractValidator<CreateViolationDto>
+    {
+        public CreateViolationDtoValidator()
+        {
+            RuleFor(x => x.ExamUserId)
+                .NotEmpty()
+                .WithMessage("ExamUserId is required.");
+            RuleFor(x => x.ViolationType)
+                .NotEmpty()
+                .WithMessage("ViolationType is required.")
+                .Must(type => Enum.TryParse(type, true, out ViolationType _))
+                .WithMessage("ViolationType must be a valid ViolationType.");
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .WithMessage("Description is required.")
+                .MaximumLength(500)
+                .WithMessage("Description must not exceed 500 characters.");
+        }
+    }
+}
