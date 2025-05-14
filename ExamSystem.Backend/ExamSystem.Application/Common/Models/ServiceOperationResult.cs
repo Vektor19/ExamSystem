3ddf310193efe266dd00fdf0e2e306309c2beebf
Vektor19 <viktor.komissarov@knu.ua>
2025-05-14 19:47:50 +0300

    feat: implement ServiceOperationResult and ServiceOperationResult<T> classes for standardized operation results

---
+++ ExamSystem.Backend/ExamSystem.Application/Common/Models/ServiceOperationResult.cs
@@ -0,0 +1,33 @@
+﻿namespace ExamSystem.Core.Common
+{
+    public class ServiceOperationResult
+    {
+        public bool Success { get; private set; }
+        public string? ErrorMessage { get; private set; }
+        public ServiceOperationErrorType? ErrorType { get; private set; }
+        private ServiceOperationResult(bool success, string? errorMessage = null, ServiceOperationErrorType? errorType = null)
+        {
+            Success = success;
+            ErrorMessage = errorMessage;
+            ErrorType = errorType;
+        }
+        public static ServiceOperationResult Ok() => new(true);
+        public static ServiceOperationResult Fail(string error, ServiceOperationErrorType type) => new(false, error, type);
+    }
+    public class ServiceOperationResult<T>
+    {
+        public bool Success { get; private set; }
+        public string? ErrorMessage { get; private set; }
+        public ServiceOperationErrorType? ErrorType { get; private set; }
+        public T? Data { get; private set; }
+        private ServiceOperationResult(bool success, T? data = default, string? errorMessage = null, ServiceOperationErrorType? errorType = null)
+        {
+            Success = success;
+            Data = data;
+            ErrorMessage = errorMessage;
+            ErrorType = errorType;
+        }
+        public static ServiceOperationResult<T> Ok(T data) => new(true, data);
+        public static ServiceOperationResult<T> Fail(string error, ServiceOperationErrorType type) => new(false, default, error, type);
+    }
+}
