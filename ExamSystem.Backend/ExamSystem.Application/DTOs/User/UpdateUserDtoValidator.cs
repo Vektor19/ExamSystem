86c20c344d84cd28846b03e3054201cff8d7d60a
Vektor19 <viktor.komissarov@knu.ua>
2025-05-14 13:26:14 +0300

    feat: add UpdateUserDtoValidator for user update validation

---
+++ ExamSystem.Backend/ExamSystem.Application/DTOs/User/UpdateUserDtoValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace ExamSystem.Application.DTOs.User
+{
+    public class UpdateUserDtoValidator : AbstractValidator<UpdateUserDto>
+    {
+        public UpdateUserDtoValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .WithMessage("First name is required.")
+                .Length(2, 50)
+                .WithMessage("First name must be between 2 and 50 characters.");
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .WithMessage("Last name is required.")
+                .Length(2, 50)
+                .WithMessage("Last name must be between 2 and 50 characters.");
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email is required.")
+                .EmailAddress()
+                .WithMessage("Invalid email format.");
+        }
+    }
+}
