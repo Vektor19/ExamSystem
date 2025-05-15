66a8ada1cb02a527f2cc9241530e47b48ca79ec2
Vektor19 <viktor.komissarov@knu.ua>
2025-05-15 15:45:01 +0300

    feat: add RemoveParticipantByEmailDto and its validator for email-based participant removal

---
+++ ExamSystem.Backend/ExamSystem.Application/DTOs/Exam/RemoveParticipantByEmailDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ExamSystem.Application.DTOs.Exam
+{
+    public class RemoveParticipantByEmailDto
+    {
+        public string Email { get; set; } = string.Empty;
+    }
+}
---
+++ ExamSystem.Backend/ExamSystem.Application/DTOs/Exam/RemoveParticipantByEmailDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using ExamSystem.Application.DTOs.Exam;
+using FluentValidation;
+
+namespace ExamSystem.Application.DTOs.User
+{
+    public class RemoveParticipantByEmailDtoValidator : AbstractValidator<RemoveParticipantByEmailDto>
+    {
+        public RemoveParticipantByEmailDtoValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email is required.")
+                .EmailAddress()
+                .WithMessage("Invalid email format.");
+        }
+    }
+}
