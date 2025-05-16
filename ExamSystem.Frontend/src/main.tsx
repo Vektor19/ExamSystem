a7e2772e00daf2e6c49d9248ba54c8af76482bff
Vektor19 <viktor.komissarov@knu.ua>
2025-05-17 00:34:15 +0300

    fix: replace BrowserRouter with HashRouter for routing

--- ExamSystem.Frontend/src/main.tsx
+++ ExamSystem.Frontend/src/main.tsx
@@ -2,16 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./Styles/index.css";
 import App from "./Components/App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./Components/Theme/theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/ExamSystem">
+    <HashRouter basename="/ExamSystem">
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
