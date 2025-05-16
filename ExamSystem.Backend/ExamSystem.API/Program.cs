b3e46619081ea9a4661f820c42e21098c2d3d9fa
Vektor19 <viktor.komissarov@knu.ua>
2025-05-16 18:30:26 +0300

    fix: ensure database migration is performed before seeding data

--- ExamSystem.Backend/ExamSystem.API/Program.cs
+++ ExamSystem.Backend/ExamSystem.API/Program.cs
@@ -1,6 +1,7 @@
 using ExamSystem.Application;
 using ExamSystem.Infrastructure;
 using ExamSystem.Persistence;
+using Microsoft.EntityFrameworkCore;
 using SQLitePCL;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -41,6 +42,10 @@ app.UseSwaggerUI();
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
+
+    var dbContext = services.GetRequiredService<ExamSystemDbContext>();
+    await dbContext.Database.MigrateAsync();
+
     var seeder = services.GetRequiredService<DbSeeder>();
     await seeder.SeedAsync();
 }
