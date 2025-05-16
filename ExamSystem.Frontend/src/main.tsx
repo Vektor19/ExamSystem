020c28f1801f9a5500d626e5cb2169c91652f071
Vektor19 <viktor.komissarov@knu.ua>
2025-05-16 20:38:43 +0300

    fix: set basename for BrowserRouter and configure base path in Vite

--- ExamSystem.Frontend/src/main.tsx
+++ ExamSystem.Frontend/src/main.tsx
@@ -8,7 +8,7 @@ import theme from "./Components/Theme/theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ExamSystem">
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
