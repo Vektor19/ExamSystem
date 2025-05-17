736152748cb3fb56e518e626873ff0ac2e228c5e
Vektor19 <viktor.komissarov@knu.ua>
2025-05-17 20:25:31 +0300

    fix: remove basename from HashRouter configuration

--- ExamSystem.Frontend/src/main.tsx
+++ ExamSystem.Frontend/src/main.tsx
@@ -8,7 +8,7 @@ import theme from "./Components/Theme/theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter basename="/ExamSystem">
+    <HashRouter>
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
